f8afdf16c94c02c5648e78f4eabd7a75d0d539d5
desbest <afaninthehouse@gmail.com>
2023-04-18 11:52:32 +0100

    update the url in credits to new author's website

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
  * @subpackage Greyomatic
  */
 ?>
-<div id="footer"><p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo('title'); ?></a> &middot; Powered by <a title="WordPress" href="http://wordpress.org">WordPress</a> and developed by <a title="Andrei Luca" href="http://students.info.uaic.ro/~andrei.luca/">Andrei Luca</a> for you.</p></div>
+<div id="footer"><p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo('title'); ?></a> &middot; Powered by <a title="WordPress" href="http://wordpress.org">WordPress</a> and developed by <a title="Andrei Luca" href="https://andreiluca.com">Andrei Luca</a> for you.</p></div>
 </div><!-- /#wrapper -->
 
 <?php wp_footer(); ?>
